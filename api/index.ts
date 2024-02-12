e7d9753d9484cd306a96b196888885a51e78a203
karlosvas <126987511+karlosvas@users.noreply.github.com>
2024-02-12 13:51:19 +0100

    Fixed API TS

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = 3000;
 
@@ -12,9 +14,20 @@ app.get('/', (_req, res: express.Response) => {
    res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/movies', async (_req, res: express.Response) => {
-   const movies = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7be72508776961f3948639fbd796bccd&page=1`)
-   return res.json(movies);
+app.post('/api/movies', (req: express.Request, res: express.Response) => {
+   // Take info 
+   const dataJSON = JSON.parse(fs.readFileSync(__dirname + '/movies.json', 'utf8'));
+   const { movies }: { movies: Array<Record<string, string>> } = dataJSON;
+   const search = req.body.search
+
+   // Filter with user input.
+   const regex = new RegExp(`^${search}`, 'i');
+   const collectionMovies = movies.filter((element) => {
+      return element.title.match(regex);
+   });
+
+   const moviesObject = { movies: collectionMovies };
+   res.status(200).send(moviesObject);
 });
 
 app.listen(PORT, () => {
